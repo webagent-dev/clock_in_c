6f278200d681e7d56f9d25c0a412bec520cd8a3b
webagent-dev <wizkenny019@gmail.com>
2022-11-04 18:53:40 +0100

    finish prioject with getch compatable error

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <curses.h>
 
+void get_time();
 int main()
 {
-    printf("Hello world!\n");
+    printf("please enter your current time in this format: <HOURS>:<MUNITE>:<SECONDS>\n");
+    get_time();
     return 0;
+}
+// function to get input and out for our time
+
+void get_time()
+{
+    int hours;
+    int minutes;
+    int seconds;
+    printf("Please Enter Your Current Hours!:");
+    scanf("%d", &hours);
+      printf("Please Enter Your Current Minute!:");
+      scanf("%d", &minutes);
+        printf("Please Enter Your Current Seconds!:");
+        scanf("%d", &seconds);
+        printf("your current time is %02d:%02d:%02d", hours, minutes, seconds);
+    // we use while loop after getting all the value from the input
+    while(hours < 24)
+    {
+             while(minutes < 60)
+              {
+                     while(seconds <  60)
+                      {
+                            sleep(1000);
+                              seconds++;
+                              printf("Time Is Now: %02d : %02d : %02d,", hours, minutes, seconds );
+                    }
+              minutes++;
+              seconds = 0;
+             }
+             hours++;
+             minutes = 0;
+    }
+    getch();
 }
